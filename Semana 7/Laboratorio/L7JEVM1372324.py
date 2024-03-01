9ffc9ef0e3c2ca7329fe2a08cad20363567bcf2e
Jevm25 <157128416+Jevm25@users.noreply.github.com>
2024-03-01 08:25:45 -0600

    Add files via upload

---
+++ Semana 7/Laboratorio/L7JEVM1372324.py
@@ -0,0 +1,50 @@
+# print("hola mundo")
+# print("Ingrese su nombre: ")
+# nombre = input()
+
+# print("Mi nombre es: " + nombre)
+
+# print("Ingrese su nombre: ")
+# nombre = input()
+# print("Hi Muggle " + nombre + " que pases una excelente dìa <3")
+
+# Ejercicio 2
+# print("Ingrese su nombre: ")
+# nombre = input()
+# print("Ingresa tu edad: ")
+# edad = int(input())
+# print("Hi bro " + nombre + " tienes " + str(edad) + " años <3")
+
+# Ejercicio 3
+# F = C *9/5 + 32
+
+# print("Ingrese la temperatura en grados C: ")
+# c = int(input())
+
+# F = (c * 9/5 + 32)
+
+# print("La temperatura en F es : " + str(F))
+
+# Ejercico 4
+
+# print("Ingresar numero")
+# numero1 = int(input())
+# print("Ingresar segundo numero")
+# numero2 = int(input())
+
+# suma = (numero1 + numero2)
+# resta = (numero1 - numero2)
+# multiplicacion = (numero1 * numero2)
+# division = (numero1 / numero2)
+
+# print("Suma = " + str(suma) + ",Resta = " + str(resta) + ",Multi. = " + str(multiplicacion) + ",Division = " + str(division))
+
+# Ejercicio 5
+
+print("Ingresar base: ")
+base = int(input())
+print("Ingrese altura: ")
+altura = int(input())
+
+area = (base*altura/2)
+print("El area es: " + str(area))
