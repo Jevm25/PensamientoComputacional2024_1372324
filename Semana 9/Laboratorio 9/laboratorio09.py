225d15cc625ca1a932720fc5fe7cde45d7602829
Jevm25 <157128416+Jevm25@users.noreply.github.com>
2024-04-05 08:09:57 -0600

    Add files via upload

---
+++ Semana 9/Laboratorio 9/laboratorio09.py
@@ -0,0 +1,57 @@
+# x = 5
+#if x == %:
+ #   print("ES mayor que 5")
+#else:
+#    print("no es mayor a 5")
+
+#print("SWITCH CASE")
+
+#VOCAL = 'a'
+
+#match vocal:
+#    case 
+
+#Ejercicio 1
+#print("Ingrese sus edad porfavor")
+#edad = int(input())
+
+#if edad >=18:
+    #print("Necesita tramitar su DPI, ya es mayor de edad")
+#else:
+    #print("Usted es menor de edad regrese despues...")
+
+#Ejercicio 2
+#print("Ingrese cualquier numero :)")
+#numero = int(input())
+
+#if numero >=0:
+    #print("Este es un numero positivo")
+#else:
+    #print("Este es un numero negativo")
+
+#Ejercicio 3
+#print("Ingrese un numero del 1 al 5")
+#numero = int(input())
+
+#numero == 1 or 2 or 3 or 4 or 5
+#match numero:
+#    case 1:
+#        print("UNO")
+#    case 2:
+#        print("DOS")
+#    case 3:
+#        print("TRES")
+#    case 4:
+#        print("CUATRO")
+#    case 5:
+#        print("CINCO")
+#    case _:
+#        print("Número no definido")
+
+#Ejercicio 4
+print("Estas son las tablas de multiplicar del 2")
+contador = 1
+while contador <= 10:
+    tabla = contador*2
+    print("2 x "+ str(contador)+ " = "+ str(tabla))
+    contador += 1
