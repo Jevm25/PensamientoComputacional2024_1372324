285621764485c4db5ee0c5352e04677a2fe81230
Jevm25 <157128416+Jevm25@users.noreply.github.com>
2024-05-03 08:25:00 -0600

    Add files via upload

---
+++ Semana 17/Laboratorio/Actividad semana 17.py
@@ -0,0 +1,94 @@
+#def suma(a,b):
+ #   return a + b
+#
+#resultado = suma(5,19)
+
+#print("Resultado suma: "+ str(resultado))
+
+#EJERCICIO 1
+#print("Dame 3 lados de un triangulo")
+#print("Ingrese lado 1")
+#lado1 = str(input())
+#print("Ingrese lado 2")
+#lado2 = str(input())
+#print("Ingrese lado 3")
+#lado3 = str(input())
+
+#def triangulo(lado1, lado2, lado3):
+#    if (lado1 == lado2 == lado3):
+#        return "Equilatero"
+#   elif (lado1 == lado2 or lado2 == lado3 or lado1 == lado3):
+#        return "Isoceles"
+#    else:
+#        (lado1 != lado2 != lado3)
+#        return "Escaleno"
+    
+#print("El triangulo que me diste es: " + triangulo(lado1, lado2, lado3))
+
+#EJERCICIO 2
+#import math
+
+#math.pi 
+#print("Porfavor ingrese el radio de un circulo")
+#radio = int(input())
+
+#def ObtenerPerimetro(r):
+#    perimetro = float(2*r)*math.pi
+#    return (perimetro)
+    
+#def ObtenerArea(r):
+#    area = pow(r,2)*math.pi 
+#    return area
+
+##def ObtenerVolumen(r):
+#    volumen = (4*math.pi*pow(r,3))/3
+#    return volumen
+
+#print("El perimetro: "+ str(ObtenerPerimetro(radio)) + " El area: "+ str(ObtenerArea(radio)) + " El volumen: "+str(ObtenerVolumen(radio)))
+
+#EJRCICIO 3
+print("Ingrese un numero del mes")
+mes = int(input())
+def Meses(m):
+    m = 1
+    return "Enero"
+def Meses(m):
+    m = 2
+    return "Febrero"
+def Meses(m):
+    m = 3
+    return "Marzo"
+def Meses(m):
+    m = 4
+    return "Abril"
+def Meses(m):
+    m = 5
+    return "Mayo"
+def Meses(m):
+    m = 6
+    return "Junio"
+def Meses(m):
+    m = 7
+    return "Julio"
+def Meses(m):
+    m = 8
+    return "Agosto"
+def Meses(m):
+    m = 9
+    return "Semptiempre"
+def Meses(m):
+    m = 10
+    return "Octubre"
+def Meses(m):
+    m = 11
+    return "Novimebre"
+def Meses(m):
+    m = 11
+    return "Diciembre"
+def Meses(m):
+    m != range[1,13]
+    return "Colocar un numero de mes valido"
+
+print("El mes es: "+ Meses(mes))
+
+
